f6c9af821ab0824ab84d956ed3086dfb03c53866
FaysalShuvo <shuvonma57@gmail.com>
2021-03-19 01:56:45 +0600

    card finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <>
-        <h1>FUck m</h1>
-        
-      </>
-    </div>
+    <>
+      <Header />
+      <Home />
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+import { Button, Nav, Navbar } from "react-bootstrap";
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="/home">Let's Go Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#destination">Destination</Nav.Link>
+          <Nav.Link href="#blog">Blog</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Button variant="warning">Log In</Button>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
